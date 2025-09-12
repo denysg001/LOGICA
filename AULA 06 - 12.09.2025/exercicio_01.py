207afb380d33bd5d1953dcfbde22bc0b95de1551
Denys Gonçalves <161529989+denysg001@users.noreply.github.com>
2025-09-12 23:10:39 +0000

    salvar

---
+++ AULA 06 - 12.09.2025/exercicio_01.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite o 1º número: ")) # Lê o primeiro número 
+n2 = int(input("Digite o 2º número: ")) # Lê o segundo número
+
+while n1 != 0:
+    n1 + n2
+    print (f"A soma dos números é: {n1 + n2}")
+    n1 = int(input("Digite o 1º número: ")) # Lê o primeiro número 
+    n2 = int(input("Digite o 2º número: ")) # Lê o segundo número
+print ("Programa encerrado")
