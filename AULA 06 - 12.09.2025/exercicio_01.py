818a4c333c1856d59966f4688967a3f7e0bdad6e
Denys Gonçalves <161529989+denysg001@users.noreply.github.com>
2025-09-13 00:10:22 +0000

    salvar

--- AULA 06 - 12.09.2025/exercicio_01.py
+++ AULA 06 - 12.09.2025/exercicio_01.py
@@ -1,11 +1,9 @@
-n1 = int(input("Digite o 1º número: ")) # Lê o primeiro número 
-n2 = int(input("Digite o 2º número: ")) # Lê o segundo número
-
-while n1 != 0: # Continua enquanto n1 for diferente de 0
-    n1 + n2 # Realiza a soma dos números
-
-    print (f"A soma dos números é: {n1 + n2}") # Exibe a soma dos números
-    
-    n1 = int(input("Digite o 1º número: ")) # Lê o primeiro número 
-    n2 = int(input("Digite o 2º número: ")) # Lê o segundo número
-print ("Programa encerrado") # Exibe mensagem de encerramento do programa
+while True:
+    print("Para encerrar o programa, digite 0 como primeiro número")
+    n1 = int(input("Digite o 1º número: ")) # Lê o primeiro número
+    if n1 == 0: # Verifica se o primeiro número é 0
+        break # Sai do loop se for 0
+    else:
+        n2 = int(input("Digite o 2º número: ")) # Lê o segundo número
+        print (f"A soma dos números é: {n1 + n2}") # Exibe a soma dos números
+print ("Programa encerrado, voce digitou 0 como primeiro número") # Exibe mensagem de encerramento do programa
